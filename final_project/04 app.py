f63c9bc5a443bc4f1979f7e428c60b93b51fde65
jake.m.brehm@gmail.com <jake.m.brehm@gmail.com>
2023-05-06 18:44:07 +0000

    added forecasting and residual plot

--- final_project/04 app.py
+++ final_project/04 app.py
@@ -39,11 +39,12 @@ PROMOTE_MODEL = False
 # COMMAND ----------
 
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import holidays
 import folium
 import mlflow
+import plotly.express as px
 import pyspark.sql.functions as F
 import pytz
 from mlflow.tracking.client import MlflowClient
@@ -96,7 +97,22 @@ weather_df = (
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC # Create gold table
+# MAGIC # Load the silver modeling table
+
+# COMMAND ----------
+
+# Load the silver modeling historical table
+silver_historical_modeling_df = (
+    spark
+    .read
+    .format("delta")
+    .load(SILVER_MODELING_HISTORICAL_PATH)
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Create gold tables
 
 # COMMAND ----------
 
@@ -154,12 +170,85 @@ gold_df = (
     .withColumnRenamed("lon", "longitude")
     # Extract only the appropriate columns
     .select(
-        "station_id", "name", "latitude", "longitude", "capacity", "num_docks_available", "num_docks_disabled", "total_docks", "num_bikes_available", "num_ebikes_available", "num_bikes_disabled",
+        "station_id", "name", "latitude", "longitude", "capacity", "num_docks_available",
+        "num_docks_disabled", "total_docks", "num_bikes_available", "num_ebikes_available",
+        "num_bikes_disabled",
     )
     # Join with the current weather table
     .join(
         current_weather_df, "name"
     )
+)
+
+# COMMAND ----------
+
+gold_forecast_df = (
+    weather_df
+    # Create new "temp" columns for each temperature unit
+    .withColumnRenamed("temp", "tempK")
+    .withColumn("tempC", col("tempK") - 273.15)
+    .withColumn("tempF", (9/5) * col("tempC") + 32)
+    # Create new "feels_like" columns for each temperature unit
+    .withColumnRenamed("feels_like", "feels_likeK")
+    .withColumn("feels_likeC", col("feels_likeK") - 273.15)
+    .withColumn("feels_likeF", (9/5) * col("feels_likeC") + 32)
+    # Convert "wind_speed" from m/s to mph
+    .withColumnRenamed("wind_speed", "wind_speed_mps")
+    .withColumn("wind_speed_mph", col("wind_speed_mps") * 2.23694)
+    # Convert "dt" from unix time to a timestamp and extract components
+    .withColumn("dt", from_unixtime("dt").cast("timestamp"))
+    .withColumn("dt_date", to_date(col("dt")))
+    .withColumn("dt_month", F.month(col("dt_date")))
+    .withColumn("dt_hour", hour(col("dt")))
+    .withColumn("dt_minute", minute(col("dt")))
+    .withColumn("dt_second", second(col("dt")))
+    # Rename the data evolution "_rescued_data" column
+    .withColumnRenamed("_rescued_data", "weather._rescued_data")
+    # Extract only the columns relevant to modeling
+    .select(
+        col("dt_date"),
+        col("dt_hour"),
+        col("tempF"),
+        col("feels_likeF"),
+        col("`rain.1h`"),
+    )
+    # Group by date and hour
+    .groupBy(
+        col("dt_date"),
+        col("dt_hour"),
+    )
+    # Perform aggregations
+    .agg(
+        F.avg("tempF").alias("temperature"),
+        F.avg("feels_likeF").alias("feels_like"),
+        F.avg("`rain.1h`").alias("precipitation"),
+    )
+    # Combine the date and hour columns into datetime format
+    .withColumn("ds", F.concat_ws(" ", "dt_date", "dt_hour"))
+    .withColumn("ds", F.to_timestamp("ds", "yyyy-MM-dd H"))
+    # Fill any null values with 0
+    .na.fill(0, ["precipitation"])
+    # Drop any unnecessary columns (done this way to keep pipeline dynamic)
+    .drop(
+        "dt_date",
+        "dt_hour",
+    )
+    # Order by datetime
+    .orderBy(col("ds"), ascending=True)
+)
+
+# COMMAND ----------
+
+gold_availability_df = (
+    status_data
+    .filter(col("station_id") == station_id)
+    .withColumn("dt", from_unixtime("last_reported"))
+    .select(
+        "dt",
+        "num_bikes_available",
+        "num_ebikes_available",
+    )
+    .withColumn("bikes_available", col("num_bikes_available") + col("num_ebikes_available"))
 )
 
 # COMMAND ----------
@@ -354,120 +443,148 @@ displayHTML(f"""
 
 # COMMAND ----------
 
-silver_weather_df = (
-    weather_df
-    # Create new "temp" columns for each temperature unit
-    .withColumnRenamed("temp", "tempK")
-    .withColumn("tempC", col("tempK") - 273.15)
-    .withColumn("tempF", (9/5) * col("tempC") + 32)
-    # Create new "feels_like" columns for each temperature unit
-    .withColumnRenamed("feels_like", "feels_likeK")
-    .withColumn("feels_likeC", col("feels_likeK") - 273.15)
-    .withColumn("feels_likeF", (9/5) * col("feels_likeC") + 32)
-    # Convert "wind_speed" from m/s to mph
-    .withColumnRenamed("wind_speed", "wind_speed_mps")
-    .withColumn("wind_speed_mph", col("wind_speed_mps") * 2.23694)
-    # Convert "dt" from unix time to a timestamp and extract components
-    .withColumn("dt", from_unixtime("dt").cast("timestamp"))
-    .withColumn("dt_date", to_date(col("dt")))
-    .withColumn("dt_month", F.month(col("dt_date")))
-    .withColumn("dt_hour", hour(col("dt")))
-    .withColumn("dt_minute", minute(col("dt")))
-    .withColumn("dt_second", second(col("dt")))
-    # Rename the data evolution "_rescued_data" column
-    .withColumnRenamed("_rescued_data", "weather._rescued_data")
-)
+# MAGIC %md
+# MAGIC ## Preparing the data
 
 # COMMAND ----------
 
-aggregation_df = (
-    silver_weather_df
-    # Extract only the columns relevant to modeling
-    .select(
-        col("dt_date"),
-        col("dt_hour"),
-        col("tempF"),
-        col("feels_likeF"),
-        col("`rain.1h`"),
-    )
-    # Group by date and hour
-    .groupBy(
-        col("dt_date"),
-        col("dt_hour"),
-    )
-    # Perform aggregations
-    .agg(
-        F.avg("tempF").alias("temperature"),
-        F.avg("feels_likeF").alias("feels_like"),
-        F.avg("`rain.1h`").alias("precipitation"),
-    )
-    # Combine the date and hour columns into datetime format
-    .withColumn("ds", F.concat_ws(" ", "dt_date", "dt_hour"))
-    .withColumn("ds", F.to_timestamp("ds", "yyyy-MM-dd H"))
-    # Fill any null values with 0
-    .na.fill(0, ["precipitation"])
-    # Drop any unnecessary columns (done this way to keep pipeline dynamic)
-    .drop(
-        "dt_date",
-        "dt_hour",
-    )
-    # Order by datetime
-    .orderBy(col("ds"), ascending=True)
-)
-
-display(aggregation_df)
+# MAGIC %md
+# MAGIC In this section, we will prepare the data for forecasting, and then prepare the forecast for plotting. A visualization of the forecast will be shown in the next section.
 
 # COMMAND ----------
 
+# Load the production model
 model_production_uri = "models:/{model_name}/production".format(model_name=ARTIFACT_PATH)
-
 print("Loading registered model version from URI: '{model_uri}'".format(model_uri=model_production_uri))
-
 model_production = mlflow.prophet.load_model(model_production_uri)
+print("Successfully loaded the production model.\n")
 
-model_staging_uri = "models:/{model_name}/staging".format(model_name=ARTIFACT_PATH)
-
-print("Loading registered model version from URI: '{model_uri}'".format(model_uri=model_staging_uri))
-
-model_staging = mlflow.prophet.load_model(model_staging_uri)
-
-# COMMAND ----------
-
-future = model_staging.make_future_dataframe(periods=4, freq="H")
-future["ds"] = future["ds"] + pd.DateOffset(hours=5)
-future
-
-# COMMAND ----------
-
-silver_historical_modeling_df = (
-    spark
-    .read
-    .format("delta")
-    .load(SILVER_MODELING_HISTORICAL_PATH)
-)
+# Try to load the staging model
+try:
+    model_staging_uri = "models:/{model_name}/staging".format(model_name=ARTIFACT_PATH)
+    print("Loading registered model version from URI: '{model_uri}'".format(model_uri=model_staging_uri))
+    model_staging = mlflow.prophet.load_model(model_staging_uri)
+except mlflow.exceptions.MlflowException:
+    # Staging model might not have been created yet
+    print("No staging model was found.")
+    model_staging = None
 
 # COMMAND ----------
 
-min_ds = future["ds"].min()
-max_ds = future["ds"].max()
+# Get the earliest time in the historical data
+start_time = silver_historical_modeling_df.orderBy("ds", ascending=True).limit(1).collect()[0]["ds"]
+
+# Get a datetime object of 4 hours in the future from the current time
+end_time = datetime.now() + timedelta(hours=4)
+
+# Create a date range by hour from the start of the data to 4 hours from now
+prediction_ds = pd.date_range(start=start_time, end=end_time, freq='H')
+# Correct for timezones
+prediction_ds = prediction_ds - pd.DateOffset(hours=5)
 
 # COMMAND ----------
 
-test_df = (
+# Get the earliest timestamp
+min_ds = prediction_ds.min()
+# Get the latest timestamp
+max_ds = prediction_ds.max()
+
+# COMMAND ----------
+
+# Columns of the dataframes must be in the same order to perform a union
+column_order = ["ds", "temperature", "feels_like", "precipitation"]
+
+# Get the appropriate data from the historical table
+historical_test_df = (
     silver_historical_modeling_df
     .filter((min_ds <= col("ds")) & (col("ds") <= max_ds))
     .drop("y")
+    .select(column_order)
 )
 
-display(test_df)
+# Get the appropriate data from the unseen table
+unseen_test_df = (
+    gold_forecast_df
+    .filter((min_ds <= col("ds")) & (col("ds") <= max_ds))
+    .select(column_order)
+)
 
 # COMMAND ----------
 
-forecast = model_staging.predict(test_df.toPandas())
+# The union of the two dataframes will be used for predictions
+prediction_df = historical_test_df.union(unseen_test_df).orderBy("ds", ascending=False)
 
 # COMMAND ----------
 
-forecast[forecast["ds"] >= (max_ds - pd.DateOffset(hours=5+4))]["yhat"]
+# Use the production model to make predictions
+forecast_df = model_production.predict(prediction_df.toPandas())
+
+# Correct for timezones
+forecast_df["ds"] = forecast_df["ds"] + pd.DateOffset(hours=5)
+
+# COMMAND ----------
+
+# Determine the number of bikes available at the station before forecasting
+bikes_available = bike_info["num_bikes_available"] + bike_info["num_ebikes_available"]
+
+# Get the last four hours worth of predictions
+net_changes = forecast_df.tail(4)["yhat"].tolist()
+
+# Use the net change forecast to determine how many bikes will be at the station
+bikes_forecast = []
+for net_change in net_changes:
+    previous_availability = bikes_available if not bikes_forecast else bikes_forecast[-1]
+    # Add the previous availability to the net change (rounded towards zero)
+    bikes_forecast.append(previous_availability + int(net_change))
+
+# COMMAND ----------
+
+# Pull the station's capacity from the gold table
+capacity = gold_df.collect()[0]["capacity"]
+print(f"Our station has a capacity of {capacity} bikes.")
+
+# COMMAND ----------
+
+# Look back four hours before the current time
+last_four_hours = forecast_df.iloc[-8:-4]["ds"]
+
+# Get the inventory at various points over those four hours
+historical_inventory_df = (
+    gold_availability_df
+    .filter((last_four_hours.min() <= col("dt")) & (col("dt") <= last_four_hours.max()))
+    .withColumnRenamed("dt", "time")
+    .withColumnRenamed("bikes_available", "inventory")
+    .select("time", "inventory")
+    .orderBy("time", ascending=True)
+)
+
+# COMMAND ----------
+
+# Create a dataframe to store the forecasted data
+forecasted_inventory_df = pd.DataFrame({
+    "time": forecast_df.tail(4)["ds"].tolist(),
+    "inventory": bikes_forecast,
+})
+
+# COMMAND ----------
+
+# Concatenate the two dataframes to prepare for plotting
+production_forecast_df = pd.concat([
+    historical_inventory_df.toPandas().rename({"dt": "time", "bikes_available": "inventory"}),
+    forecasted_inventory_df
+], axis=0)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Visualizing the forecast
+
+# COMMAND ----------
+
+fig = px.line(production_forecast_df, x="time", y="inventory", title="Forecast of Bike Inventory")
+fig.add_hline(y=0, line_width=3, line_dash="dash", line_color="gray")
+fig.add_hline(y=capacity, line_width=3, line_dash="dash", line_color="gray")
+fig.show()
 
 # COMMAND ----------
 
@@ -476,19 +593,67 @@ forecast[forecast["ds"] >= (max_ds - pd.DateOffset(hours=5+4))]["yhat"]
 
 # COMMAND ----------
 
-# import matplotlib.pyplot as plt
+# MAGIC %md
+# MAGIC Now, we can plot the residuals for both the production and staging forecasts. Only the historical time period is plotted due to lack of data, but this should be enough to provide a general idea of how well the models are performing.
+# MAGIC
+# MAGIC If there is no model currently staged, the visualization will only include residuals for the production model.
 
-# # Get the residuals for the staging model
-# staging_residuals = staging_predictions_df - gold_df["actual_availability"].values.reshape(-1, 1)
+# COMMAND ----------
 
-# # Plot the residuals
-# fig, ax = plt.subplots()
-# ax.plot(staging_residuals)
-# ax.axhline(y=0, color="gray", linestyle="--")
-# ax.set_xlabel("Time")
-# ax.set_ylabel("Residual")
-# ax.set_title("Staging Model Residual Plot")
-# plt.show()
+# Get the actual net change values from the historical modeling table
+actual_df = silver_historical_modeling_df.toPandas()[["ds", "y"]].reset_index(drop=True)
+
+# Instantiate a list to store the resulting dataframes for production and staging
+result_dfs = []
+
+# Get the forecasts for the historical time period
+production_forecast = forecast_df[(actual_df["ds"].min() <= forecast_df["ds"]) & (forecast_df["ds"] <= actual_df["ds"].max())]
+
+# Calculate the residuals for the production forecast
+results_production = pd.DataFrame({
+    "yhat": production_forecast["yhat"],
+    "residual": production_forecast["yhat"] - actual_df["y"],
+    "label": "Production",
+})
+result_dfs.append(results_production)
+
+# Only plot staging residuals if there is a staging model
+if model_staging:
+    # Use the production model to make predictions
+    staging_forecast_df = model_staging.predict(prediction_df.toPandas())
+
+    # Correct for timezones
+    staging_forecast_df["ds"] = staging_forecast_df["ds"] + pd.DateOffset(hours=5)
+
+    # Get the forecasts for the historical time period
+    staging_forecast = staging_forecast_df[(actual_df["ds"].min() <= staging_forecast_df["ds"]) & (staging_forecast_df["ds"] <= actual_df["ds"].max())]
+
+    # Calculate the residuals for the staging forecast
+    results_staging = pd.DataFrame({
+        "yhat": staging_forecast["yhat"],
+        "residual": staging_forecast["yhat"] - actual_df["y"],
+        "label": "Staging",
+    })
+    result_dfs.append(results_staging)
+else:
+    print("The residual plot will only contain production data because there is currently no staging model.")
+
+# Concatenate the results in preparation for plotting
+results = pd.concat(result_dfs, axis=0, ignore_index=True)
+
+# COMMAND ----------
+
+# Plot the residuals
+fig = px.scatter(
+    results,
+    x="yhat",
+    y="residual",
+    marginal_y="violin",
+    trendline="ols",
+    color="label",
+    opacity=0.5 
+)
+fig.show()
 
 # COMMAND ----------
 
@@ -502,20 +667,30 @@ forecast[forecast["ds"] >= (max_ds - pd.DateOffset(hours=5+4))]["yhat"]
 
 # COMMAND ----------
 
-# Use the previously instantiated client to transition
-if PROMOTE_MODEL:
+# Promote the staging model if desired
+if PROMOTE_MODEL and model_staging:
+
+    # Get the current production model
+    latest_production = [model for model in latest_versions if model.current_stage=="Production"][0]
+
     # Archive the current production model
     client.transition_model_version_stage(
         name=latest_production.name,
         version=latest_production.version,
         stage="Archived",
     )
+
+    # Get the current staging model
+    latest_staging = [model for model in latest_versions if model.current_stage=="Staging"][0]
+
     # Promote the staging model to production
     client.transition_model_version_stage(
         name=latest_staging.name,
         version=latest_staging.version,
         stage="Production",
     )
+
+    print("The staging model has been successfully promoted to production.")
 
 # COMMAND ----------
 
